379c409cc7d40acaf68132413cca972bd0df56f0
Lenovo <Lenovo@DESKTOP-Q74AOCJ>
2022-10-21 11:36:51 +0530

    h

---
+++ Main/src/Hrishi/hrishi.java
@@ -0,0 +1,10 @@
+package Hrishi;
+
+public class hrishi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Main/src/Main/Main.java
+++ Main/src/Main/Main.java
@@ -1,11 +1,18 @@
 package Main;
-
+import java.util.Scanner;
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("hello");
+public static void main(String[] args) {
+	System.out.println("Role number");
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+	if(a%4 == 1 || a== 1) {
+		System.out.println("Sapphire group");
+	}else if(a%4 == 2 || a== 2) {
+		System.out.println("Peral group");
+	}else if(a%4 == 2 || a== 2) {
+		System.out.println("Ruby group");
+	}else if(a%4 == 2 || a== 2) {
+		System.out.println("Emerald group");
 	}
-
+}
 }
